17ae10977197af079606124c38f4f42f42e46cbc
BK ^^ <84545556+Barikeloo@users.noreply.github.com>
2022-02-05 12:02:25 +0100

    Se ha mejorado el rendimiento del CarHud

--- bk-CarHud/main.lua
+++ bk-CarHud/main.lua
@@ -1,62 +1,34 @@
-JB = {
-    sendNUI = SendNUIMessage,
-    thread  = CreateThread,
-    sleep   = function(msec)
-        return Wait(msec)
-    end,
-    playerPed = function()
-        return PlayerPedId()
-    end,
-    getVehicle = function()
-        return GetVehiclePedIsIn(JB.playerPed())
-    end,
-    getSpeed = function()
-        return GetEntitySpeed(JB.playerPed());
-    end,    
-    getVehClass = function()
-        return GetVehicleClass(GetVehiclePedIsIn(JB.playerPed(), false));
-    end,    
-    isInVehicle = function()
-        return IsPedInAnyVehicle(JB.playerPed());
-    end,    
-    setVehicleSeat = function(seat)
-        return GetPedInVehicleSeat(JB.getVehicle(), seat);
-    end,
-    speedoMeter = {
-        Init = function()
-            while true do 
-                local inBike = false 
-                local inAnyCar = false
-                local sleep = 1000
-                local km = (JB.getSpeed()* 3.6)
-                if JB.isInVehicle() then 
-                    cinturon = false
-                    sleep = 100
-                    inBike = false 
-                    inAnyCar = false
-                    local vc = JB.getVehClass()
-                    if((vc >= 0 and vc <= 12)) then
-                        inAnyCar = true
-                    elseif(vc == 13) then
-                        inBike = true
-                    end
-                    JB.sendNUI({
-                        action = "speedometer";
-                        fuel   = GetVehicleFuelLevel(JB.getVehicle());
-                        damage = GetVehicleEngineHealth(JB.getVehicle());
-                        engine    = GetVehicleCurrentGear(JB.getVehicle());
-                        on = GetIsVehicleEngineRunning(JB.getVehicle());
-                        speed  = km;
-                        Bici = inBike;
-                    })
-                else
-                    JB.sendNUI({
-                        action = 'hideSpeedo';
-                    })
+CreateThread(function()
+        while true do 
+            local inBike = false 
+            local inAnyCar = false
+            local sleep = 1000
+            local speed = GetEntitySpeed(PlayerPedId())
+            local km = (speed * 3.6)
+            if IsPedInAnyVehicle(PlayerPedId()) then 
+                sleep = 100
+                inBike = false 
+                inAnyCar = false
+                local vc = GetVehicleClass(GetVehiclePedIsIn(PlayerPedId(), false))
+                if((vc >= 0 and vc <= 12)) then
+                    inAnyCar = true
+                elseif(vc == 13) then
+                    inBike = true
                 end
-                JB.sleep(sleep)
+                SendNUIMessage({
+                    action = "speedometer";
+                    fuel   = GetVehicleFuelLevel(GetVehiclePedIsIn(PlayerPedId()));
+                    damage = GetVehicleEngineHealth(GetVehiclePedIsIn(PlayerPedId()));
+                    engine    = GetVehicleCurrentGear(GetVehiclePedIsIn(PlayerPedId()));
+                    on = GetIsVehicleEngineRunning(GetVehiclePedIsIn(PlayerPedId()));
+                    speed  = km;
+                    Bici = inBike;
+                })
+            else
+                SendNUIMessage({
+                    action = 'hideSpeedo';
+                })
             end
-        end,
-    }
-}
-JB.thread(JB.speedoMeter.Init)
+            Wait(sleep)
+        end
+end)
